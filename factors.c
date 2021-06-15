3749ec922e21c8744992239c906132c7e6857a39
Seb Asus VS <sebas93cay@gmail.com>
2021-06-15 18:10:42 -0500

    Save and print numbers with two unsigned long long ints

--- factors.c
+++ factors.c
@@ -1,13 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+#include "head_factors.h"
+
+void str2llu(char *buff, unsigned long long int *n)
+{
+    int number_lenght;
+    char lost_digit;
+
+    number_lenght = strlen(buff) - 1;
+
+    if (number_lenght <= NUMBER_FRAC_SIZE)
+    {
+        n[1] = strtol(buff, NULL, 10);
+        n[0] = 0;
+    }
+    else
+    {
+        n[1] = strtol(buff + number_lenght - NUMBER_FRAC_SIZE, NULL, 10);
+        lost_digit = buff[number_lenght - NUMBER_FRAC_SIZE];
+        buff[number_lenght - NUMBER_FRAC_SIZE] = '\0';
+        n[0] = strtol(buff, NULL, 10);
+        buff[number_lenght - NUMBER_FRAC_SIZE] = lost_digit;
+    }
+}
+
+char *llu2str(unsigned long long int *n)
+{
+    char *number_text = NULL;
+    unsigned int i, counter = 0;
+
+    number_text = calloc(LLINT_ARRAY_SIZE * NUMBER_FRAC_SIZE, sizeof(char));
+    if (number_text == NULL)
+        return (NULL);
+
+    for (i = 0; i < LLINT_ARRAY_SIZE; i++)
+        lu2str(n[i], number_text, &counter);
+
+    return (number_text);
+}
+
+void lu2str(unsigned long long int n, char *buff, unsigned int *counter)
+{
+    int digit;
+
+    if (n == 0)
+        return;
+
+    digit = n % 10;
+    n = n / 10;
+    lu2str(n, buff, counter);
+    buff[*counter] = digit + '0';
+    *counter += 1;
+}
 
 int main(int argc, char **argv)
 {
     FILE *file = NULL;
-    char *buff = NULL, *end_conf;
+    char *buff = NULL, *number_str;
     size_t buff_size = 0;
-    unsigned long long int n = 0;
+    unsigned long long int n_array[LLINT_ARRAY_SIZE] = {0, 0};
 
     if (argc != 2)
         fprintf(stderr, "Usage factors <file>\n"), exit(1);
@@ -15,12 +65,11 @@ int main(int argc, char **argv)
     file = fopen(argv[1], "r");
     while (getline(&buff, &buff_size, file) != EOF)
     {
-        buff[strlen(buff) - 1] = 'a';
-        end_conf = buff;
-        n = strtol(buff, &end_conf, 10);
-        printf("%s -> %llu\n", buff, n);
-        printf("end: %s\n", end_conf);
+        str2llu(buff, n_array);
+        number_str = llu2str(n_array);
+        printf("%s -> %s\n", buff, number_str);
     }
-
+    free(buff);
+    free(number_str);
     return (0);
 }
--- head_factors.h
+++ head_factors.h
@@ -1,4 +1,15 @@
 #ifndef FACTORS_H
 #define FACTORS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUMBER_FRAC_SIZE 18
+#define LLINT_ARRAY_SIZE 2
+
+void str2llu(char *buff, unsigned long long int *n);
+char *llu2str(unsigned long long int *n);
+void lu2str(unsigned long long int n, char *buff, unsigned int *counter);
+
 #endif
